3bfdb3793197e3e7c596d6950d0c29078ba7202b
Khirish7 <khirish1311@gmail.com>
2022-12-23 00:27:36 +0530

    finished EarlySalaryiOS SDK

---
+++ EarlySalaryiOSSDK/EarlySalaryiOSSDK/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  EarlySalaryiOSSDK
+//
+//  Created by Khirish Meshram on 23/12/22.
+//
+
+import Foundation
+
+public class Constants {
+    
+    public static var botUrl = ""
+    public static var botId = ""
+    public static var accessToken = ""
+    public static var customerId = ""
+    public static var isTokenVerify = false
+    public static let tokenVerificationUrl = "/chat/verify-access-token/"
+    public static let livechatSessionIDVerificationUrl = "/livechat/get-livechat-session-expire/"
+    public static var mobileChatbotSessionID = ""
+    public static var mobileLiveChatSessionID = ""
+    
+}
---
+++ EarlySalaryiOSSDK/EarlySalaryiOSSDK/EarlySalaryiOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  EarlySalaryiOSSDK.h
+//  EarlySalaryiOSSDK
+//
+//  Created by Khirish Meshram on 23/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EarlySalaryiOSSDK.
+FOUNDATION_EXPORT double EarlySalaryiOSSDKVersionNumber;
+
+//! Project version string for EarlySalaryiOSSDK.
+FOUNDATION_EXPORT const unsigned char EarlySalaryiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EarlySalaryiOSSDK/PublicHeader.h>
+
+
